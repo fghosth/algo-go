352efedb0a5964eee5223a3c8b279d327cb3a165
Derek fan <fghosth@163.com>
2018-01-12 16:45:38 +0800

    test

--- test/rbtree_test.go
+++ test/rbtree_test.go
@@ -1,6 +1,11 @@
 package algo_test
 
 import (
+	"fmt"
+	"log"
+	_ "net/http/pprof"
+	"os"
+	"runtime/pprof"
 	"testing"
 )
 
@@ -8,5 +13,16 @@ func init() {
 
 }
 func TestRBTree(t *testing.T) {
-
+	num := uint32(1000000000)
+	var id [1000000000]uint32
+	for i := uint32(0); i < num; i++ {
+		id[i] = i
+	}
+	fmt.Println(id[3000000])
+	fm, err := os.OpenFile("./tmp/mem.out", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.WriteHeapProfile(fm)
+	fm.Close()
 }
