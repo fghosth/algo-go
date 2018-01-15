8f6d0f93bc75c1515815f4bfa1d8573857cb65da
Derek fan <fghosth@163.com>
2018-01-15 12:12:54 +0800

    test

--- test/roaringBM_test.go
+++ test/roaringBM_test.go
@@ -3,7 +3,10 @@ package algo_test
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/RoaringBitmap/roaring"
 )
@@ -12,47 +15,94 @@ func init() {
 
 }
 
-func TestRoaringBitmap(t *testing.T) {
-	// example inspired by https://github.com/fzandona/goroar
-	fmt.Println("==roaring==")
-	rb1 := roaring.BitmapOf(1, 2, 3, 4, 5, 100, 1000)
-	fmt.Println(rb1.String())
-
-	rb2 := roaring.BitmapOf(3, 4, 1000)
-	fmt.Println(rb2.String())
-
-	rb3 := roaring.NewBitmap()
-	fmt.Println(rb3.String())
-
-	fmt.Println("Cardinality: ", rb1.GetCardinality())
-
-	fmt.Println("Contains 3? ", rb1.Contains(3))
-
-	rb1.And(rb2)
-
-	rb3.Add(1)
-	rb3.Add(5)
-
-	rb3.Or(rb1)
-
-	// computes union of the three bitmaps in parallel using 4 workers
-	ParOr(4, rb1, rb2, rb3)
-	// computes intersection of the three bitmaps in parallel using 4 workers
-	ParAnd(4, rb1, rb2, rb3)
-
-	// prints 1, 3, 4, 5, 1000
-	i := rb3.Iterator()
-	for i.HasNext() {
-		fmt.Println(i.Next())
+func read3(path string) []byte {
+	fi, err := os.Open(path)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println()
-
-	// next we include an example of serialization
-	buf := new(bytes.Buffer)
-	rb1.WriteTo(buf) // we omit error handling
-	newrb := roaring.NewBitmap()
-	newrb.ReadFrom(buf)
-	if rb1.Equals(newrb) {
-		fmt.Println("I wrote the content to a byte stream and read it back.")
-	}
+	defer fi.Close()
+	fd, err := ioutil.ReadAll(fi)
+	return fd
 }
+
+func TestRoaringB(t *testing.T) {
+	rb := roaring.NewBitmap()
+	rb2 := roaring.NewBitmap()
+	// count := uint32(1000000000)
+	// for i := uint32(0); i < count; i++ {
+	// 	rb.Add(i)
+	// 	rb2.Add(i + 2)
+	// }
+	// buf := new(bytes.Buffer)
+	// rb.WriteTo(buf)                                     // we omit error handling
+	// err2 := ioutil.WriteFile("./rb", buf.Bytes(), 0666) //写入文件(字节数组)
+	// if err2 != nil {
+	// 	fmt.Println(err2)
+	// }
+	//
+	// buf2 := new(bytes.Buffer)
+	// rb2.WriteTo(buf2)                                     // we omit error handling
+	// err3 := ioutil.WriteFile("./rb2", buf2.Bytes(), 0666) //写入文件(字节数组)
+	// if err3 != nil {
+	// 	fmt.Println(err3)
+	// }
+	t1 := time.Now() // get current time
+	buff := new(bytes.Buffer)
+	buff.Write(read3("./rb"))
+	rb.ReadFrom(buff)
+
+	buff2 := new(bytes.Buffer)
+	buff2.Write(read3("./rb2"))
+	rb2.ReadFrom(buff2)
+
+	roaring.ParOr(4, rb, rb2)
+	// fmt.Println("rb1.And(rb2)", c.String())
+	elapsed := time.Since(t1)
+	fmt.Println("App elapsed: ", elapsed)
+}
+
+// func TestRoaringBitmap(t *testing.T) {
+// 	// example inspired by https://github.com/fzandona/goroar
+// 	fmt.Println("==roaring==")
+// 	rb1 := roaring.BitmapOf(1, 2, 3, 4, 5, 100, 1000)
+// 	fmt.Println(rb1.String())
+// 	fmt.Println(rb1.Select(2))
+// 	rb2 := roaring.BitmapOf(3, 4, 1000)
+// 	fmt.Println(rb2.String())
+//
+// 	rb3 := roaring.NewBitmap()
+// 	fmt.Println(rb3.String())
+//
+// 	fmt.Println("Cardinality: ", rb1.GetCardinality())
+//
+// 	fmt.Println("Contains 3? ", rb1.Contains(3))
+//
+// 	rb1.And(rb2)
+// 	fmt.Println("rb1.And(rb2)", rb1.String())
+// 	rb3.Add(1)
+// 	rb3.Add(5)
+//
+// 	rb3.Or(rb1)
+// 	fmt.Println("rb3.Or(rb1)", rb3.String())
+// 	// computes union of the three bitmaps in parallel using 4 workers
+// 	c := roaring.ParOr(4, rb1, rb2, rb3)
+// 	fmt.Println("roaring.ParOr(4, rb1, rb2, rb3)", c.String())
+// 	// computes intersection of the three bitmaps in parallel using 4 workers
+// 	d := roaring.ParAnd(8, rb1, rb2, rb3)
+// 	fmt.Println("roaring.ParAnd(4, rb1, rb2, rb3)", d.String())
+// 	// prints 1, 3, 4, 5, 1000
+// 	i := rb3.Iterator()
+// 	for i.HasNext() {
+// 		fmt.Println(i.Next())
+// 	}
+// 	fmt.Println()
+//
+// 	// next we include an example of serialization
+// 	buf := new(bytes.Buffer)
+// 	rb1.WriteTo(buf) // we omit error handling
+// 	newrb := roaring.NewBitmap()
+// 	newrb.ReadFrom(buf)
+// 	if rb1.Equals(newrb) {
+// 		fmt.Println("I wrote the content to a byte stream and read it back.")
+// 	}
+// }
